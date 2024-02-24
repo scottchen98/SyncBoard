521eb1f71990f3b6b8e9d307d36efe436e152866
scottchen98 <scott.chen1117@gmail.com>
2024-02-24 15:31:26 -0800

    refactor code and add database configuration

--- syncboard.Web/Program.cs
+++ syncboard.Web/Program.cs
@@ -1,4 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SyncBoard.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+builder.Services.AddDbContext<DatabaseContext>(
+    opt =>
+    {
+        opt.UseNpgsql(connectionString);
+        if (builder.Environment.IsDevelopment())
+        {
+            opt
+              .LogTo(Console.WriteLine, LogLevel.Information)
+              .EnableSensitiveDataLogging()
+              .EnableDetailedErrors();
+        }
+    }
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
