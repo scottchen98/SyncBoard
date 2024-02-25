d47e7b2aebf66963b6146ec6c2d1deec1ef77811
scottchen98 <scott.chen1117@gmail.com>
2024-02-25 13:48:13 -0800

    configure application to use controllers and Swagger in development

--- syncboard.Web/Program.cs
+++ syncboard.Web/Program.cs
@@ -19,7 +19,20 @@ builder.Services.AddDbContext<DatabaseContext>(
     }
 );
 
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
