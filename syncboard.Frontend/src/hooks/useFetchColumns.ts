5807e29c8fe2da8a800b8369700f1c6f1ffb01e5
scottchen98 <scott.chen1117@gmail.com>
2024-02-26 17:54:37 -0800

    create useFetchColumns hook

---
+++ syncboard.Frontend/src/hooks/useFetchColumns.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+import type { Column } from "../types";
+
+export default function useFetchPolls() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["columns"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:5173/api/Columns");
+      const columns: Column[] = await response.json();
+      if (!columns || !columns.length) return [];
+      return columns;
+    },
+  });
+
+  return { columns: data, isLoading, error };
+}
