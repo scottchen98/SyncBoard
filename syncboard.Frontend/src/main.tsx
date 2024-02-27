b7e3aa67fe013a11de7bce1d0f4e7dfdd224edf8
scottchen98 <scott.chen1117@gmail.com>
2024-02-26 17:14:03 -0800

    set up tanstack query and react query devtools

--- syncboard.Frontend/src/main.tsx
+++ syncboard.Frontend/src/main.tsx
@@ -1,5 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+// Create a client
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+);
