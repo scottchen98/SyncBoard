e490576bf77c59f4991e672e939a7a6ecde5a84a
scottchen98 <scott.chen1117@gmail.com>
2024-02-29 17:35:19 -0800

    add id column to UpdateCardDto

--- syncboard.Web/Dtos/UpdateCardDto.cs
+++ syncboard.Web/Dtos/UpdateCardDto.cs
@@ -2,6 +2,7 @@ namespace SyncBoard.Dtos;
 
 public class UpdateCardDto
 {
+    public int Id { get; set; }
     public string Content { get; set; } = "";
     public int Position { get; set; }
     public int ColumnId { get; set; }
