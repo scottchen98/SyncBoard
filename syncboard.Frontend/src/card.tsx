6381580676a35a67e6077c6ad1cceedbd1295fe2
scottchen98 <scott.chen1117@gmail.com>
2024-03-02 17:06:47 -0800

    create handleDeleteCard function

--- syncboard.Frontend/src/card.tsx
+++ syncboard.Frontend/src/card.tsx
@@ -1,7 +1,9 @@
 import { Draggable } from "react-beautiful-dnd";
 import { Trash2 } from "lucide-react";
+import { useQueryClient } from "@tanstack/react-query";
 
-import type { Card } from "./types";
+import type { Card, Column } from "./types";
+import useDeleteCard from "./hooks/useDeleteCard";
 
 export default function CardItem({
   card,
@@ -10,19 +12,49 @@ export default function CardItem({
   card: Card;
   index: number;
 }) {
+  const { id: cardId, columnId, content } = card;
+  const { deleteCard } = useDeleteCard();
+  const qc = useQueryClient();
+
+  const handleDeleteCard = (cardId: Card["id"], columnId: Column["id"]) => {
+    const columns = qc.getQueryData<Column[]>(["columns"]);
+    if (!columns) return;
+
+    // find the column that the card belongs to based on the columnId
+    const column = columns.find((column) => column.id === columnId);
+    if (!column) return columns;
+
+    // Remove the card from the column and update the position of the remaining cards
+    const updatedCards = column.cards
+      .filter((card) => card.id !== cardId)
+      .map((card, index) => ({ ...card, position: index }));
+
+    const newColumns = [...columns];
+    // Replace the old column with the new updated column
+    newColumns.splice(columnId - 1, 1, {
+      ...column,
+      cards: updatedCards,
+    });
+    deleteCard({ cardId, newColumns });
+  };
+
   return (
-    <Draggable draggableId={`${card.id}`} index={index}>
+    <Draggable draggableId={`${cardId}`} index={index}>
       {(provided) => (
         <div
-          className="mb-2 w-full break-words rounded-lg border-none bg-slate-50 p-2"
+          className="mb-2 min-h-10 w-full break-words rounded-lg border-none bg-slate-50 p-2"
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <button className="float-right" aria-label="Delete card">
+          <button
+            className="float-right"
+            aria-label="Delete card"
+            onClick={() => handleDeleteCard(cardId, columnId)}
+          >
             <Trash2 className="text-red-500" size={24} />
           </button>
-          {card.content}
+          {content}
         </div>
       )}
     </Draggable>
