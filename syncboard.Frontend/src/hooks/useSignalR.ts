94dc3121d70e285b286203d485d0d5c6436c305f
scottchen98 <scott.chen1117@gmail.com>
2024-03-03 22:06:09 -0800

    create useSignalR hook for basic boilerplate code

---
+++ syncboard.Frontend/src/hooks/useSignalR.ts
@@ -0,0 +1,62 @@
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from "@microsoft/signalr";
+import { useEffect, useState } from "react";
+
+export default function useSignalR(url: string) {
+  const [connection, setConnection] = useState<HubConnection | undefined>();
+
+  useEffect(() => {
+    let canceled = false;
+    const connection = new HubConnectionBuilder()
+      .withUrl(url)
+      .withAutomaticReconnect()
+      .configureLogging(LogLevel.Information)
+      .build();
+
+    connection
+      .start()
+      .then(() => {
+        if (!canceled) {
+          setConnection(connection);
+        }
+      })
+      .catch((error) => {
+        console.log("signal error", error);
+      });
+
+    connection.onclose(() => {
+      if (canceled) {
+        return;
+      }
+      console.log("signal closed");
+      setConnection(undefined);
+    });
+
+    connection.onreconnecting(() => {
+      if (canceled) {
+        return;
+      }
+      console.log("signal reconnecting");
+      setConnection(undefined);
+    });
+
+    connection.onreconnected(() => {
+      if (canceled) {
+        return;
+      }
+      console.log("signal reconnected");
+      setConnection(connection);
+    });
+
+    // Clean up the connection when the component unmounts
+    return () => {
+      canceled = true;
+      connection.stop();
+    };
+  }, [url]);
+
+  return { connection };
+}
