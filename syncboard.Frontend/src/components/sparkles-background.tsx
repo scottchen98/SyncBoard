8f5284d1f6f3c03bd3bd00545bf5f1edc3b1ba75
scottchen98 <scott.chen1117@gmail.com>
2024-03-04 16:06:37 -0800

    fix text size and viewport height

--- syncboard.Frontend/src/components/sparkles-background.tsx
+++ syncboard.Frontend/src/components/sparkles-background.tsx
@@ -8,7 +8,7 @@ export function SparklesBackground({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex h-dvh w-full flex-col items-center justify-start overflow-hidden rounded-md bg-black">
+    <div className="relative flex min-h-dvh w-full flex-col items-center justify-start overflow-hidden rounded-md bg-black pb-16">
       <div className="absolute inset-0 h-screen w-full">
         <SparklesCore
           id="tsparticlesfullpage"
@@ -20,7 +20,7 @@ export function SparklesBackground({
           particleColor="#FFFFFF"
         />
       </div>
-      <h1 className="relative z-20 my-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-6xl">
+      <h1 className="relative z-20 my-20 text-center text-5xl font-bold text-white md:text-6xl lg:text-7xl">
         SyncBoard
       </h1>
       <div className="z-10 w-full">{children}</div>
